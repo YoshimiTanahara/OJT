70c8cbd3e35940fc52bdd0f2c602c66db132647b
棚原　嘉 <ysm.t02@gmail.com>
2018-12-11 22:31:27 +0900

    swiperとfooterの導入 まだ動かない

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.2/css/swiper.min.css">
     <title>Document</title>
 </head>
 <body>
@@ -12,18 +13,29 @@
       <div class="logo">
         <img src="./img/logo.png" alt="">
       </div>
-      <div class="visual">
-        <img src="./img/visual_1_pc.png" alt="">
-          <p>進化し続ける「街」アメリカンビレッジマガジン</p>
-      </div>
       <div class="text">
         <img src="./img/visual_text_pc.png" alt="">
       </div>
+
+      <!-- swiper -->
+
+  <div class="swiper-wrapper">
+    <div class="visual swiper-slide">
+      <img src="./img/visual_1_pc.png" alt="">
+        <p class="info">進化し続ける「街」アメリカンビレッジマガジン</p>
+    </div>
+  </div>
     </section>
   </main>
 
-  <?php get_template_part('page', 'article'); ?>
+<!-- <?php get_template_part('page', 'article'); ?> -->
 
-  <footer></footer>
+  <footer>Copyright © Shinohara. ALL RIGHTS RESERVED.
+  </footer>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.2/js/swiper.min.js"></script>
+  <script src="app.js"></script>
 </body>
 </html>
