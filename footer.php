df2dff730bd43f9a345e2ecad2cc3da4e5a17d9c
棚原　嘉 <ysm.t02@gmail.com>
2018-12-12 10:49:42 +0900

    メディアクエリ

---
+++ footer.php
@@ -0,0 +1,9 @@
+  <footer>Copyright © Shinohara. ALL RIGHTS RESERVED.
+    <?php wp_footer(); ?>
+  </footer>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.4.2/js/swiper.min.js"></script>
+  <script src="app.js"></script>
+</body>
+</html>
